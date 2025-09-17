7f70c1a5269710cdc08a96028aa0e3fe184b39d2
Dimitrios Avdis <dimitrios.avdis@1und1.de>
2025-09-17 10:12:55 +0200

    increase db defaults

--- app/db.py
+++ app/db.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    pool_size=20,  # Increase pool size as needed
+    max_overflow=40,  # Allow more overflow connections
 )
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
